a5f876a8e08f4857ca5969bdffe7170e72e900e2
Rots87 <ncanas@openmailbox.org>
2016-07-12 14:42:22 -0600

    Ejercicios básicos de declaración de variables e info de los tipos de datos

---
+++ Consola/EjercicioNumerosEnteros.java
@@ -0,0 +1,15 @@
+public class EjercicioNumerosEnteros  {
+  public static void main(String[] args) {
+      byte j=0; //1 Bytes
+      short i=0; //2 Bytes
+      int k=0; //4 Bytes
+      long l=0; //8 Bytes
+
+      //Ejemplos de declaracion de variables
+
+      byte edad = 22;
+      short year = 2016;
+      int id = 4587654;
+      long hora = 9999999;
+  }
+}
