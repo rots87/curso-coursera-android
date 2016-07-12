a742db077ed26a6711c56424b8333d2dcb208794
Rots87 <ncanas@openmailbox.org>
2016-07-12 14:52:10 -0600

    Ejercicios de numeros flotantes y doubles

---
+++ Consola/EjerciciosNumerosFlotantes.java
@@ -0,0 +1,6 @@
+public class EjerciciosNumerosFlotantes{
+  public static void main(String[] args) {
+    float estatura = 1.80F; // Siempre es necesario poner la F para distiguirlo del Double
+    double dinero = 932.556647;
+    }
+}
