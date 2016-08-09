e91b922a7ee9f71a245905fd1cd27481cef9f96f
Rots87 <ncanas@openmailbox.org>
2016-08-09 13:28:23 -0600

    Se coloco un boton para que se finalice la ejecucion del programa. Metodo nuevo:     public void actionPerformed(java.awt.event.ActionEvent e)

--- IDE/HolaMundoJswing/src/holamundojswing/HolaMundoJswing.java
+++ IDE/HolaMundoJswing/src/holamundojswing/HolaMundoJswing.java
@@ -4,21 +4,38 @@
  * and open the template in the editor.
  */
 package holamundojswing;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 /**
  *
  * @author Rots87
  */
-public class HolaMundoJswing extends JFrame{
+
+
+public class HolaMundoJswing extends JFrame implements ActionListener{
     private final JLabel label1;
+    private final JButton boton1;
     
     public HolaMundoJswing(){
         setLayout(null);
         label1=new JLabel("Hola Mundo.");
         label1.setBounds(10,20,200,30);
+        boton1 = new JButton();
+        boton1.setBounds(10,40,200,30);
+        boton1.setToolTipText("Clic para cerrar");
         add(label1);
-    }
+        add(boton1);
+        boton1.addActionListener(this);
+      }
+    
+    @Override
+    public void actionPerformed(java.awt.event.ActionEvent e) {
+            if (e.getSource()==boton1) {
+                System.exit(0);
+            }
+        }
+        
     /**
      * @param args the command line arguments
      */
@@ -27,5 +44,4 @@ public class HolaMundoJswing extends JFrame{
         formulario1.setBounds(10, 10, 400, 300);
         formulario1.setVisible(true);
     }
-    
 }
