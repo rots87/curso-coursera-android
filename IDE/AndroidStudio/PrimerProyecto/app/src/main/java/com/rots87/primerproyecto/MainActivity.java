8bc2ad2c97aaccafdd4166c3444e6307856aee97
Nestor Cañas <ncanas@openmailbox.org>
2016-08-16 00:10:24 -0600

    primer proyecto de android, semana 3

---
+++ IDE/AndroidStudio/PrimerProyecto/app/src/main/java/com/rots87/primerproyecto/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rots87.primerproyecto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
