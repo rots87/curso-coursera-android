768dcc6eb416353793f0b7b4cb9c9fe898a0c999
Nestor Cañas <ncanas@openmailbox.org>
2016-09-28 23:12:54 -0600

    Tarea 2 del curso de android. AppBar Terminada (solo el view(

---
+++ IDE/AndroidStudio/Mascotas/app/src/main/java/com/rots87/mascotas/AppBar.java
@@ -0,0 +1,13 @@
+package com.rots87.mascotas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppBar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_bar);
+    }
+}
