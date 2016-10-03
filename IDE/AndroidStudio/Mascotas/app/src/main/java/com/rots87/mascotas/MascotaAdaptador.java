45e81637bfb2d6414afdbc4dee44fd50e195131b
Nestor Cañas <ncanas@openmailbox.org>
2016-10-02 23:36:23 -0600

    Tarea 2

---
+++ IDE/AndroidStudio/Mascotas/app/src/main/java/com/rots87/mascotas/MascotaAdaptador.java
@@ -0,0 +1,8 @@
+package com.rots87.mascotas;
+
+/**
+ * Created by rots on 10-02-16.
+ */
+
+public class MascotaAdaptador {
+}
--- IDE/AndroidStudio/Mascotas/app/src/main/java/com/rots87/mascotas/Raiting.java
+++ IDE/AndroidStudio/Mascotas/app/src/main/java/com/rots87/mascotas/Raiting.java
@@ -3,11 +3,11 @@ package com.rots87.mascotas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Favoritas extends AppCompatActivity {
+public class Raiting extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_favoritas);
+        setContentView(R.layout.activity_raiting);
     }
 }
