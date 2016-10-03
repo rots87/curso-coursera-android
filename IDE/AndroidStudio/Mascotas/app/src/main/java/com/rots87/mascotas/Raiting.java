f31d731554d43bc4ed9320cdee14bb87f5c98958
Nestor Cañas <ncanas@openmailbox.org>
2016-10-02 23:52:49 -0600

    añadido el boton de back en el segundo activity

--- IDE/AndroidStudio/Mascotas/app/src/main/java/com/rots87/mascotas/Raiting.java
+++ IDE/AndroidStudio/Mascotas/app/src/main/java/com/rots87/mascotas/Raiting.java
@@ -1,9 +1,11 @@
 package com.rots87.mascotas;
 
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -13,9 +15,15 @@ public class Raiting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_raiting);
-
-        //android.app.ActionBar actionBar= getActionBar();
-        //actionBar.setDisplayHomeAsUpEnabled(true);
+        Toolbar AppBar = (Toolbar) findViewById(R.id.AppBar);
+        setSupportActionBar(AppBar);
+        AppBar.setNavigationIcon(R.drawable.abc_ic_ab_back_material);
+        AppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                regresarMain(v);
+            }
+        });
 
         ImageView Fav = (ImageView) findViewById(R.id.abFav);
         Fav.setVisibility(View.INVISIBLE);
